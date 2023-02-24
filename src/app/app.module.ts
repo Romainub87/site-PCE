547891fcba27d4375dddfa8dbd1623a5ebfcce10
Romainub87 <romaincordier33160@gmail.com>
2023-02-24 13:43:09 +0100

    component hotel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HotelComponent } from './hotel/hotel.component';
 
 @NgModule({
   declarations: [
     AccueilComponent,
-    NavbarComponent
+    NavbarComponent,
+    HotelComponent
   ],
   imports: [
     BrowserModule,
