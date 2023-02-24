a349d5de0d2b93a3b8b4d282bb12c07af85923b4
Romainub87 <romaincordier33160@gmail.com>
2023-02-24 15:26:50 +0100

    meteo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HotelComponent } from './hotel/hotel.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AccueilComponent,
     NavbarComponent,
-    HotelComponent
+    HotelComponent,
+    MeteoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AccueilComponent]
