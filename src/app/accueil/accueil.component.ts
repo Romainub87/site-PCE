4d037c619a055912f88aca80786ba06881a42ce7
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-03-07 23:34:52 +0100

    ajout backgrounds

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        const body = document.getElementsByTagName('div')[0];
+        body.className = this.getClassNameFromUrl(url);
+      }
+    });
+
+  }
+
+  private getClassNameFromUrl(url: string): string {
+    if (url.includes('/accueil')) {
+      return 'accueil';
+    } else if (url.includes('/resto')) {
+      return 'resto';
+    } else if (url.includes('/reserver')) {
+      return 'reserver';
+    } else if (url.includes('/contact')) {
+      return 'contact';
+    } else {
+      return '';
+    }
+  }
 
 }
